c88dbe91839365945c64734a4698c39593ba6c1d
Nirupama <niru.java@yahoo.com>
2017-12-14 22:17:25 -0500

    modified:   app.component.css 	modified:   app.component.html 	modified:   app.module.ts 	new file:   auth-check.guard.spec.ts 	new file:   auth-check.guard.ts 	new file:   dashboard/dashboard.component.css 	new file:   dashboard/dashboard.component.html 	new file:   dashboard/dashboard.component.spec.ts 	new file:   dashboard/dashboard.component.ts 	new file:   home/home.component.css 	new file:   home/home.component.html 	new file:   home/home.component.spec.ts 	new file:   home/home.component.ts 	new file:   login/login.component.css 	new file:   login/login.component.html 	new file:   login/login.component.spec.ts 	new file:   login/login.component.ts 	new file:   user.service.spec.ts 	new file:   user.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { Route } from '@angular/router/src/config';
+import { UserService } from './user.service';
+import { AuthCheckGuard } from './auth-check.guard';
 
+const appRoutes:Routes = [
+  {
+  path: '',
+  component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthCheckGuard],
+    component: DashboardComponent
+    }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
-  providers: [],
+  providers: [UserService, AuthCheckGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
